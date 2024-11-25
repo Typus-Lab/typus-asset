169e441d8eab09cdb56e6d53871d186842d998c9
jericlong <jeric.l@alphabit.software>
2024-11-25 18:43:20 +0800

    update mainnet index 89~93 receipts

--- update_receipt.sh
+++ update_receipt.sh
@@ -1,4 +1,4 @@
 rm receipt/$1/deposit/$2.jpg
 rm receipt/$1/bid/$2.jpg
-cp "$3" "receipt/$1/deposit/$2.jpg"
-cp "$4" "receipt/$1/bid/$2.jpg"
+cp ~/Downloads/receipts/$3*.jpg "receipt/$1/deposit/$2.jpg"
+cp ~/Downloads/receipts/$4*.jpg "receipt/$1/bid/$2.jpg"
